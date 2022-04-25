02cfbd9914b64dfc6a44e03f2be88ceebdcdfcaa
Reyna <mingurar@gmail.com>
2022-04-24 21:40:38 -0500

    terraform code

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "region" {
+  type    = string
+  default = "us-east-1a"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0f9fc25dd2506cf6d"
+}
+variable "instance-type" {
+  type    = string
+  default = "t2.micro"
+}
+variable "keypair" {
+  type    = string
+  default = "kubkey"
+}
+variable "name" {
+  type    = string
+  default = "Jenkins"
+}
+
+
+
+
+
+
+
